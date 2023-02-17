311dfd3906fb453810f853621fd72407dee813c0
= <=>
2023-02-17 15:09:13 +0000

    added test for Board and Cheese

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,7 @@ const sequelize = require("../src/db/connection");
 const { User } = require("../src/models/User");
 const { Cheese } = require("../src/models/Cheese");
 const { Board } = require("../src/models/Board");
-const { seedUser } = require("./seedData");
+const { seedUser, seedBoard, seedCheese } = require("./seedData");
 
 //Setup - Teardown
 beforeAll(async () => {
@@ -26,7 +26,26 @@ afterAll(async () => {
 describe("Testing the tables creation", () => {
   test("User table creation", async () => {
     const testUser = await User.create(seedUser[0]);
+    const testUser2 = await User.create(seedUser[2]);
     expect(testUser.username).toBe("Jane");
     expect(testUser.email).toBe("jane.doe@email.com");
+    expect(testUser2.username).toBe("Joe");
+    expect(testUser2.email).toBe("joe.doe@email.com");
+  });
+  test("Board table creation", async () => {
+    const testBoard = await Board.create(seedBoard[0]);
+    const testBoard2 = await Board.create(seedBoard[1]);
+    expect(testBoard.type).toBe("French");
+    expect(testBoard.rating).toBe(10);
+    expect(testBoard2.type).toBe("English");
+    expect(testBoard2.rating).toBe(5);
+  });
+  test("Cheese table creation", async () => {
+    const testCheese = await Cheese.create(seedCheese[0]);
+    const testCheese2 = await Cheese.create(seedCheese[2]);
+    expect(testCheese.title).toBe("Camembert");
+    expect(testCheese.description).toBe("something");
+    expect(testCheese2.title).toBe("Cheddar");
+    expect(testCheese2.description).toBe("some other thing");
   });
 });
--- tests/seedData.js
+++ tests/seedData.js
@@ -16,6 +16,32 @@ const seedUser = [
     email: "garry.doe@email.com",
   },
 ];
+
+const seedBoard = [
+  { type: "French", description: "Selection of French cheeses", rating: 10 },
+  { type: "English", description: "Selection of English cheeses", rating: 5 },
+];
+
+const seedCheese = [
+  {
+    title: "Camembert",
+    description: "something",
+  },
+  {
+    title: "Brie",
+    description: "something else",
+  },
+  {
+    title: "Cheddar",
+    description: "some other thing",
+  },
+  {
+    title: "Red Leicester",
+    description: "some description",
+  },
+];
 module.exports = {
   seedUser,
+  seedBoard,
+  seedCheese,
 };
